858051593d7928c94aad4678a76402dc9068a779
Hussam Alhajlah <hos200sa@gmail.com>
2021-03-13 01:38:22 +0300

    removing redux-logger from production build

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import {connect} from 'react-redux'
 
 import CollectionItem from "../../components/collection-item/collection-item.component";
-import ShopPage from "../shop/shop.component";
+// import ShopPage from "../shop/shop.component";
 
 import { selectCollection } from '../../redux/shop/shop.selectors'
 
