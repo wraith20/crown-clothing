a998908e793e1366b65af2f63b8f0861b2bafb56
Hussam Alhajlah <hos200sa@gmail.com>
2021-04-21 14:43:27 +0300

    implement code that pulls in our shop data from firestore and stores it into the hop reducer from our shop page. Also included withSpinner HOC

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,31 +1,19 @@
-import { createSelector } from "reselect";
-import memoize from "lodash.memoize";
+import { createSelector } from 'reselect';
 
-// const COLLECTION_ID_MAP = {
-//   hats: 1,
-//   sneakers: 2,
-//   jackets: 3,
-//   womens: 4,
-//   mans: 5,
-// };
-
-const selectShop = (state) => state.shop;
+const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
   [selectShop],
-  (shop) => shop.collections
+  shop => shop.collections
 );
 
 export const selectCollectionsForPreview = createSelector(
   [selectCollections],
-  collections => Object.keys(collections).map(key => collections[key])
-)
-
-export const selectCollection = memoize((collectionUrlParam) =>
-  createSelector([selectCollections], (collections) => collections[collectionUrlParam]
-    // collections.find(
-    //   (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    // )
-
-  )
+  collections => collections ? Object.keys(collections).map(key => collections[key]) : []
 );
+
+export const selectCollection = collectionUrlParam =>
+  createSelector(
+    [selectCollections],
+    collections => collections ? collections[collectionUrlParam] : null
+  );
